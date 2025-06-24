02130d45361f55871817a8b73784eb7a9a7c466d
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-06-24 20:23:20 +0300

    add css style to main

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -25,12 +25,16 @@ const student2: Student={
 const studentsList =[student1, student2];
 
 const table = document.createElement("table");
+table.style.border = "1px solid black";
+
 const headerRow=document.createElement("tr");
 
 ["First Name","Location"].forEach(
     (headerText)=>{
         const header=document.createElement("th");
         header.textContent=headerText;
+        header.style.border = "1px solid black";
+        header.style.padding = "4px";
         headerRow.appendChild(header)
     }
 );
@@ -44,7 +48,11 @@ studentsList.forEach(
        const firstName=document.createElement("td");
         const location=document.createElement("td");
              firstName.textContent= student.firstName;
+         firstName.style.border = "1px solid black";
+         firstName.style.padding = "4px";
              location.textContent= student.location;
+        location.style.border = "1px solid black";
+        location.style.padding = "4px";
        row.appendChild(firstName);
        row.appendChild(location)
        table.appendChild(row);
