efd26c1aadf909da8fcd702e3f9d9ec7471f2846
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-06-24 21:26:34 +0300

    add class StudentClass through interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -21,3 +21,27 @@ interface printTeacherFunction {
 const printTeacher:printTeacherFunction=(firstName, lastName)=> {
     return `${firstName.charAt(0)}. ${lastName}`;
 };
+
+interface StudentClassInterface {
+    firstName:string,
+    lastName:string,
+    workOnHomework(): string,
+    displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName:string, lastName:string){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        }
+
+    workOnHomework (){
+        return 'Currently working';
+    }
+    displayName(): string {
+        return `${this.firstName}`;
+    }
+}
